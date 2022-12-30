3dc2062e295352faa73b3bb22438012d06ae2f3e
Divesh Kumar Chordia <diveshkumarchordia@gmail.com>
2022-12-30 22:58:19 +0530

    Deploying

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) =>{
   loader (messageDiv);
   
   //this function is used to send the prompt as json , via POST request to the server
-const response = await fetch('http://localhost:5000', {
+const response = await fetch('https://chatgptbydivesh.onrender.com', {
   method: 'POST',
   headers: {
   'Content-Type': 'application/json'
